51fff46f54deabf162660054589372b0356947a5
Kthwaits <Kthwaits50@gmail.com>
2025-05-20 15:21:47 -0400

    Enable adding multiple ACs

--- custom_components/windmillac/__init__.py
+++ custom_components/windmillac/__init__.py
@@ -19,14 +19,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     coordinator = WindmillDataUpdateCoordinator(hass, blynk_service)
 
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN]["coordinator"] = coordinator
+    hass.data[DOMAIN][entry.entry_id] = {
+        "coordinator": coordinator,
+    }
 
     await coordinator.async_config_entry_first_refresh()
 
     for platform in PLATFORMS:
         _LOGGER.debug(f"Loading platform: {platform}")
         hass.async_create_task(
-          hass.config_entries.async_forward_entry_setup(entry, platform)
+            hass.config_entries.async_forward_entry_setup(entry, platform)
         )
 
     return True
@@ -35,6 +37,6 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
-        hass.data[DOMAIN].pop("coordinator")
+        hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
